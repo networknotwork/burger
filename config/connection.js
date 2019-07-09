313b77d778b687ec279a705ff5692a56dc6bfd5b
Rusty Boot <nicholastuso@yahoo.com>
2019-07-09 15:27:37 -0700

    Test

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection({
   host: "localhost",
-  port: 3306,
+  port: 8080,
   user: "root",
   password: "nJosepht32",
   database: "burgers_db"
